6b3f79c3773e74767072ad8ee7199db551617938
Markus L <10787350+Xernium@users.noreply.github.com>
2018-02-26 19:02:08 +0100

    Added TSSSaver-Crawler
    Go with the time!
    Doesn't use any API but should still work

--- savethemblobs.py
+++ savethemblobs.py
@@ -7,6 +7,8 @@
 # Copyright (c) 2013 Neal <neal@ineal.me>
 #   Updated 2016 iApeiron
 #       deprecated and obsolete APIs removed
+#   Upgraded/Updated 2018
+#       New TSS-Saving Solution crawler by /u/1Conan
 #
 # examples:
 #   savethemblobs.py 1050808663311 iPhone3,1
@@ -19,8 +21,9 @@ import sys, os, argparse
 import requests
 import json
 import six
+import re
 
-__version__ = '2.1'
+__version__ = '2.2'
 
 USER_AGENT = 'savethemblobs/%s' % __version__
 
@@ -60,6 +63,68 @@ def request_blobs_from_cydia(board, build, ecid, cpid=None, bdid=None):
         return { 'MESSAGE': 'TSS HTTP STATUS:', 'STATUS': r.status_code }
     return parse_tss_response(r.text)
 
+def save_tsssaver(ecid, model, boardconfig, version, buildid, savedir, overwrite):
+    url = 'https://stor.1conan.com/tsssaver/shsh/'
+    # Pre 10.2
+    f_path_arg = [
+        'apnonce-0dc448240696866b0cc1b2ac3eca4ce22af11cb3',
+        'apnonce-352dfad1713834f4f94c5ff3c3e5e99477347b95',
+        'apnonce-42c88f5a7b75bc944c288a7215391dc9c73b6e9f',
+        'apnonce-603be133ff0bdfa0f83f21e74191cf6770ea43bb',
+        'apnonce-9804d99e85bbafd4bb1135a1044773b4df9f1ba3',
+        'noapnonce'
+    ]
+    r = []
+    # Why did this have to change mid-way-through?!
+    tmp1 = version[:4]
+    if float(tmp1) <= 10.2 and not version == '10.2.1':
+        # Pre 10.2.1: EASY
+        for c in range(len(f_path_arg)):
+            filename = ('%s-%s-%s-%s_%s.shsh2' % (ecid, model, version, buildid, f_path_arg[c]))
+            save_path = os.path.join(savedir, filename)
+            req_url = ('%s%s/%s/%s/%s_%s_%s-%s.shsh2' % (url, ecid, version, f_path_arg[c], ecid, model, version, buildid))
+            r = requests.get(req_url, headers={'User-Agent': 'savethemblobs/2.1'} )
+            if r.status_code == requests.codes.ok:
+                if not (os.path.exists(save_path)) or overwrite:
+                    write_to_file(save_path, (r.text))
+                    print('TSSSaver blob saved to %s' % (save_path))
+                else:
+                    print('Blobs already exist at %s' % (save_path))
+    else:
+        # Post 10.2.1: Now we are in for a real treat...
+        dir_listing_url = ('%s%s/%s/noapnonce' % (url, ecid, version))
+        dir_r = requests.get(dir_listing_url, headers={'User-Agent': 'savethemblobs/2.1'} )
+        if dir_r.status_code == requests.codes.ok:
+            listing = dir_r.text
+            search_str = ('<a href="%s_%s_%s_%s-%s_' % (ecid, model, boardconfig, version, buildid))
+            if search_str in listing:
+                for c in re.finditer(search_str, listing):
+                    d = c.end()
+                    e = c.end()
+                    control_bit = False
+                    while '.shsh2' not in listing[d:e]:
+                        e=e+1
+                        if e>10000:
+                            print('Failed to parse TSSSaver')
+                            control_bit = True
+                            break
+                    if control_bit:
+                        return
+                    else:
+                        # Hello there nonce
+                        nonce = listing[d:(e-6)]
+                        filename = ('%s-%s-%s-%s_%s.shsh2' % (ecid, model, version, buildid, nonce))
+                        save_path = os.path.join(savedir, filename)
+                        req_url = ('%s%s/%s/noapnonce/%s_%s_%s_%s-%s_%s.shsh2' % (url, ecid, version, ecid, model, boardconfig, version, buildid, nonce))
+                        r = requests.get(req_url, headers={'User-Agent': 'savethemblobs/2.1'} )
+                        if r.status_code == requests.codes.ok:
+                            if not (os.path.exists(save_path)) or overwrite:
+                                write_to_file(save_path, (r.text))
+                                print('TSSSaver blob saved to %s' % (save_path))
+                            else:
+                                print('Blobs already exist at %s' % (save_path))
+
+
 def submit_blobs_to_cydia(cpid, bdid, ecid, data):
     url = 'http://cydia.saurik.com/tss@home/api/store/%s/%s/%s' % (cpid, bdid, ecid)
     r = requests.post(url, headers={'User-Agent': USER_AGENT}, data=data)
@@ -86,6 +151,7 @@ def parse_args():
     parser.add_argument('--overwrite-apple', help='overwrite any existing blobs (only from Apple)', action='store_true')
     parser.add_argument('--overwrite-cydia', help='overwrite any existing blobs (only from Cydia)', action='store_true')
     parser.add_argument('--no-submit-cydia', help='don\'t submit blobs to Cydia server', action='store_true')
+    parser.add_argument('--tsssaver-blobs', help='fetch shsh2 from 1Conan\'s TSSSaver (http://TSSSaver.1Conan.com)', action='store_true')
     parser.add_argument('--cydia-blobs', help='fetch blobs from Cydia server (32 bit devices only)', action='store_true')
     return parser.parse_args()
 
@@ -110,6 +176,11 @@ def main(passedArgs = None):
              return 0
 
         pass
+
+    if args.tsssaver_blobs:
+        submitcydia = False
+    else:
+        submitcydia = args.no_submit_cydia
 
     if not os.path.exists(args.save_dir):
         os.makedirs(args.save_dir)
@@ -136,7 +207,7 @@ def main(passedArgs = None):
                     print('Fresh blobs saved to %s' % (save_path))
                     write_to_file(save_path, r['REQUEST_STRING'])
 
-                    if not args.no_submit_cydia:
+                    if not submitcydia:
                         print('Submitting blobs to Cydia server')
                         submit_blobs_to_cydia(cpid, bdid, ecid, r['REQUEST_STRING'])
 
@@ -146,6 +217,15 @@ def main(passedArgs = None):
             else:
                 print('Blobs already exist at %s' % (save_path))
 
+    if args.tsssaver_blobs:
+        print('Fetching /u/1Conans TSS-SHSH2-Saver for %s with ECID %s' % (model, ecid))
+        for f in d['firmwares']:
+            # This helps reduce query-spam
+            tmp1 = f['version'][:2]
+            if '.' in tmp1:
+                tmp1 = f['version'][:1]
+            if float(tmp1) >= 10.0:
+                save_tsssaver(ecid, model, board, f['version'], f['buildid'], args.save_dir, args.overwrite)
 
     if args.cydia_blobs:
         print('Fetching blobs available on Cydia server')
